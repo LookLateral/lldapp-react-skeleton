0bd908b898dbb7c665aec181e2f6c3f63cd017e4
Simon <simone.baroni@looklateral.com>
2018-11-12 17:03:05 +0100

    updated package.json
    updated with redux dependencies

--- client/core/components/Menu.js
+++ client/core/components/Menu.js
@@ -5,7 +5,7 @@ import Toolbar from 'material-ui/Toolbar'
 //import IconButton from 'material-ui/IconButton'
 //import HomeIcon from 'material-ui-icons/Home'
 import Button from 'material-ui/Button'
-//import auth from './../auth/auth-helper'
+import auth from './../../auth/auth-helper'
 import {Link, withRouter} from 'react-router-dom'
 
 const isActive = (history, path) => {
@@ -73,6 +73,30 @@ const Menu = withRouter(({history}) => (
 			<Button style={isActive(history, "/journal" )}>JOURNAL
 			</Button>
 		</Link>
+
+    {
+        !auth.isAuthenticated() && (<span>
+          <Link to="/signup">
+            <Button style={isActive(history, "/signup")}>Sign up
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button style={isActive(history, "/signin")}>Sign In
+            </Button>
+          </Link>
+        </span>)
+      }
+      {
+        auth.isAuthenticated() && (<span>
+          <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+          </Link>
+          <Button color="inherit" onClick={() => {
+              auth.signout(() => history.push('/'))
+            }}>Sign out</Button>
+        </span>)
+      }
+
     </Toolbar>
   </AppBar>
 ))
